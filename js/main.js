bdcc08afbf6a80836da9c38b3549e73f8cfa9cc2
Zachary A. K <ZKDajeki@gmail.com>
2020-04-22 01:11:20 -0400

    Fixed Duck Sound

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let imgURLs = ["img/Page7.png", "img/CowsVoiceUnanswered.png", "img/CowsVoiceans
                 "img/ChickensVoiceUnanswered.png", "img/ChickensVoiceanswered.png", "img/Page1.png", 
                 "img/FrontCover.png", "img/Chick.png", "img/Duck.png", "img/Cow.png"];
 
-let audioURLs = ["https://freesound.org/data/previews/73/73573_877451-lq.mp3", "http://www.orangefreesounds.com/wp-content/uploads/2018/06/Duck-quacking-sound.mp3?_=1",
+let audioURLs = ["https://freesound.org/data/previews/73/73573_877451-lq.mp3", "/Sounds/Duck-quacking-sound.mp3",
                 "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-audio-hero/audio_hero_Cow_DIGIC10_23_340.mp3?_=7"];
 
 function setupBookClasses() {
@@ -88,7 +88,7 @@ function setupAnimalSounds() {
     })
 
     var duckAudio = document.createElement("audio");
-    duckAudio.src = "http://www.orangefreesounds.com/wp-content/uploads/2018/06/Duck-quacking-sound.mp3?_=1";
+    duckAudio.src = "/Sounds/Duck-quacking-sound.mp3";
     duckAudio.volume = 0.05;
     duckAudio.loop = true;
 
